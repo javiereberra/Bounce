e7f488f3f9c7d645a86bd7ea80ee5af909db2929
javiereberra <javier_berra@live.com>
2023-12-14 18:10:19 -0300

    Anotaciones en el código

--- Bounce/main.cpp
+++ Bounce/main.cpp
@@ -15,12 +15,13 @@ int main()
 	//establecer velocidad vertical//
 	float velocity(0.0f);
 
-	//establecer aceleracion en 0//
+	//establecer aceleracion de la gravedad//
 	float acceleration(0.1f);
 
 	//booleano para activar el rozamiento
 	bool rozamiento;
 
+	//empezar sin el rozamiento//
 	rozamiento = false;
 
 	while (window.isOpen())
@@ -30,7 +31,7 @@ int main()
 		{
 			if (event.type == Event::Closed)
 				window.close();
-
+			// Opción por teclado (tecla R) para activar o desactivar el rozamiento del rebote de la esfera
 
 			if (event.type == Event::KeyPressed) {
 				if (event.key.code == Keyboard::Key::R) {
@@ -41,6 +42,7 @@ int main()
 					else {
 						rozamiento = false;
 					}
+					//controlar por consola si el rozamiento está activado o no//
 					cout << rozamiento << endl;
 
 				}
@@ -49,19 +51,24 @@ int main()
 		//establecer el deltaTime a 60fps//
 		float deltaTime = 1.0f / 60.0f;
 
-		//la aceleración aplicada a la velocidad
+		//aplicar la gravedad a la velocidad
 		velocity += acceleration * deltaTime;
 
 		//aplicar el movimiento ajustando la posición a la velocidad//
 		position.y += velocity * deltaTime;
 
+
+		//condicional para aplicar el rebote con el suelo con o sin el rozamiento
+
 		if ((rozamiento) && (position.y >= 550.0f))
 		{
+			//Establecer el límite vertical o suelo
 			position.y = 550.0f;
+			//invertir el sentido de la velocidad para efectuar el rebote
 			velocity = -velocity;
+			//aquí se le aplica el rozamiento a la velocidad
 			velocity *= 0.75f;
 		}
-
 
 		if ((!rozamiento) && (position.y >= 550.0f))
 		{
@@ -71,9 +78,7 @@ int main()
 		}
 
 
-
-
-
+		//dibujar la esfera
 		window.clear();
 
 		CircleShape object;
